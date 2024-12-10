64effceaa86e062740ff0d15e88e09f8514502d4
Duu <vuhn261@gmail.com>
2024-12-10 13:35:16 +0700

    learn git later

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
-config.font_size = 14
+config.font_size = 15.5
 
 config.color_scheme = 'Catppuccin Macchiato'
 
@@ -15,6 +15,12 @@ config.keys = {
   {key="P", mods="CTRL|SHIFT", action=wezterm.action.DisableDefaultAssignment},
   -- Disable Ctrl + Shift + N
   {key="N", mods="CTRL|SHIFT", action=wezterm.action.DisableDefaultAssignment},
+}
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
 }
 
 return config
