255409fcd73e006fad33cd744282f564896685c8
Duu <vuhn261@gmail.com>
2025-01-08 20:39:14 +0700

    add hypr

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,8 +3,9 @@ local wezterm = require("wezterm")
 return {
 	color_scheme = "Catppuccin Macchiato",
 
+	enable_wayland = false,
 	font = wezterm.font("FiraCode Nerd Font Mono"),
-	font_size = 16,
+	font_size = 14,
 
 	enable_tab_bar = false,
 	window_decorations = "RESIZE",
