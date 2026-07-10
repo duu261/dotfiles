23ca8ed1cf649d86385fef6020daf89e73303789
Duu <vuhn261@gmail.com>
2026-07-11 06:02:52 +0700

    feat(hypr): wire Lua config entrypoint, remove old hyprlang .conf files

---
+++ hypr/dot-config/hypr/hyprland.lua
@@ -0,0 +1,28 @@
+-- hypr/dot-config/hypr/hyprland.lua
+
+-- Colors
+require("macchiato")
+
+-- Monitors
+require("monitors")
+
+-- Autostart
+require("autostart")
+
+-- Environment variables
+require("environment")
+
+-- Look and feel
+require("looksAndFeels")
+
+-- Input
+require("input")
+
+-- Keybindings
+require("keybindings")
+
+-- Workspaces
+require("workspaces")
+
+-- Windows
+require("windows")
